b5ebbd7a898bf86993114a848af349ca2689fb22
Vandinho <evandro.menshaid@gmail.com>
2026-05-03 19:21:57 -0300

    G9 - Trabalhando com funções

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-document.getElementById('form-user-create').addEventListener("submit", function(event){
+function addLine(dataUser){
+
+   var tr = document.createElement("tr");
+
+   tr.innerHTML = `
+      <tr>
+         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+         <td>${dataUser.name}</td>
+         <td>${dataUser.email}</td>
+         <td>${dataUser.admin}</td>
+         <td>${dataUser.birth}</td>
+         <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+         </td>
+      </tr>
+   
+   `;
+
+   document.getElementById('table-users').appendChild(tr);
+
+}
+
+document.getElementById('form-user-create').addEventListener("submit", function (event) {
 
    event.preventDefault();
 
    fields.forEach(function (field, index) {
 
-  if (field.name == "gender") {
+      if (field.name == "gender") {
 
-    if (field.checked === true) {
+         if (field.checked === true) {
 
-      user[field.name] = field.value;
+            user[field.name] = field.value;
 
-    }
+         }
 
-  } else {
+      } else {
 
-      user[field.name] = field.value;
+         user[field.name] = field.value;
 
-  }
-});
-
+      }
+   });
+   addLine(user);
 });
