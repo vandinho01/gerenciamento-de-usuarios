a9a09515b01a29aebc910c1461ebd880d5ccaf5c
Vandinho <evandro.menshaid@gmail.com>
2026-05-03 20:01:34 -0300

    G10 - Programação Orientada a Objetos - Classes e MVC

---
+++ models/User.js
@@ -0,0 +1,14 @@
+class User {
+
+    constructor(name, gender, birth, country, email, senha, password, photo, admin){
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.email = email;
+        this.senha = senha;
+        this.photo = photo;
+        this.admin = admin;
+
+    }
+
+}
