e277ea95aca8552350f25eb9e569aeec607b1db6
Vandinho <evandro.menshaid@gmail.com>
2026-05-03 16:40:44 -0300

    G3 - Variáveis

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.querySelector("#exampleInputName").value
+var nome = document.querySelector("#exampleInputName");
+var masc = document.querySelector('#exampleInputGenderM');
+var fem = document.querySelector('exampleInputGenderF');
+var nasc = document.querySelector('#exampleInputBirth');
+var pais = document.querySelector('#exampleInputCountry');
+var email = document.querySelector('#exampleInputEmail1');
+var senha = document.querySelector('exampleInputPassword1');
+var foto = document.querySelector('#exampleInputFile');
