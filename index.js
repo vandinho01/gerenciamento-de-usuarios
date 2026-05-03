0cd0073bb00cae21c14fd723ef606fadb276a883
Vandinho <evandro.menshaid@gmail.com>
2026-05-03 17:01:32 -0300

    G5 - For Each

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ var email = document.querySelector('#exampleInputEmail');
 var password = document.querySelector('exampleInputPassword');
 var photo = document.querySelector('#exampleInputFile');
 var admin = document.querySelector('#exampleInputAdmin');
+
+var fields = document.querySelectorAll('form-user-create [name]');
+
+fields.forEach(function(field, index){
+
+
+
+});
