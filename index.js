a052d0f57f3880f471d77371f39109bd4abd57db
Vandinho <evandro.menshaid@gmail.com>
2026-05-03 18:52:36 -0300

    G7 - Trabalhando com JSON

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-var birth = document.querySelector('#exampleInputBirth');
-var country = document.querySelector('#exampleInputCountry');
-var email = document.querySelector('#exampleInputEmail');
-var password = document.querySelector('exampleInputPassword');
-var photo = document.querySelector('#exampleInputFile');
-var admin = document.querySelector('#exampleInputAdmin');
+var fields = document.querySelectorAll("form-user-create [name]");
+var user = {};
 
-var fields = document.querySelectorAll('form-user-create [name]');
+fields.forEach(function (field, index) {
+   
+  if (field.name == "gender") {
 
-fields.forEach(function(field, index){
+    if (field.checked === true) {
 
-   if(field.name == "gender"){
+      user[field.name] = field.value;
 
-      if(field.checked === true){
-         console.log('SIM', field );
-      }
-        
-   } else {
-      console.log('Não');
-   }
+    }
 
+  } else {
+
+      user[field.name] = field.value;
+
+  }
 });
+
+console.log('Pasou');
+console.log('JSON ' , user);
