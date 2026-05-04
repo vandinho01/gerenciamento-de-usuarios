8ac30b92d54a12cdc52cc6af5e35049d9050deba
Vandinho <evandro.menshaid@gmail.com>
2026-05-04 20:37:40 -0300

    G14 - Trabalhando com Promise no JavaScript

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,78 +1,86 @@
 class UserController {
-  constructor(formId, tableId) {
-    this.formEl = document.getElementById(formId);
-    this.tableEl = document.getElementById(tableId);
+    constructor(formId, tableId) {
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
 
-    this.onSubmit();
-  }
+        this.onSubmit();
+    }
 
-  onSubmit() {
+    onSubmit() {
 
-    this.formEl.addEventListener("submit", event => {
-        event.preventDefault();
+        this.formEl.addEventListener("submit", event => {
+            event.preventDefault();
 
-        let values = this.getValues();
+            let values = this.getValues();
 
-        this.getPhoto((content) => {
+            this.getPhoto().then((content) => {
+                values.photo = content;
 
-            values.photo = content;
+                this.addLine(values);
 
-            this.addLine(values);
+            }, (e) => {
+                console.error(e);
+            });
+        });
+    }
+
+    getPhoto() {
+
+        return new Promise( (resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name === 'photo') {
+                    return item;
+                }
+            })
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+                reject(e);
+            }
+
+            fileReader.readAsDataURL(file);
 
         });
-      });
-  }
+    }
 
-  getPhoto(callback){
+    getValues() {
+        let user = {};
 
-    let fileReader = new FileReader();
+        [...this.formEl.elements].forEach(function (field, index) {
+            if (field.name == "gender") {
+                if (field.checked === true) {
+                    user[field.name] = field.value;
+                }
+            } else {
+                user[field.name] = field.value;
+            }
+        });
 
-    let elements = [...this.formEl.elements].filter(item=>{
-        if(item.name === 'photo'){
-            return item;
-        }
-    })
+        return new User(
+            user.name,
+            user.gender,
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
+            user.admin,
+        );
+    } //Fechando a classe GetValues
 
-    let file = elements[0].files[0];
+    addLine(dataUser) {
 
-    fileReader.onload = () => {
-
-        callback(fileReader.result);
-
-    };
-
-    fileReader.readAsDataURL(file);
-    
-
-  }
-  getValues() {
-    let user = {};
-
-    [...this.formEl.elements].forEach(function (field, index) {
-      if (field.name == "gender") {
-        if (field.checked === true) {
-          user[field.name] = field.value;
-        }
-      } else {
-        user[field.name] = field.value;
-      }
-    });
-
-    return new User(
-      user.name,
-      user.gender,
-      user.birth,
-      user.country,
-      user.email,
-      user.password,
-      user.photo,
-      user.admin,
-    );
-  } //Fechando a classe GetValues
-
-    addLine(dataUser){
-
-    this.tableEl.innerHTML = `
+        this.tableEl.innerHTML = `
         <tr>
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -87,6 +95,6 @@ class UserController {
     
     `;
 
-}
+    }
 
 }
