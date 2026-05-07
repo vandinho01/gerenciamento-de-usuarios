514bdeffa43dcf3be3e23a7ac220018be4554ebb
Vandinho <evandro.menshaid@gmail.com>
2026-05-07 14:58:03 -0300

    G24 - Trabalhando com Edição de Dados - Alterando dados do Objeto

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 class UserController {
-    constructor(formId, tableId) {
-        this.formEl = document.getElementById(formId);
+    constructor(formIdCreate, formIdUpdate, tableId) {
+        this.formEl = document.getElementById(formIdCreate);
+        this.formUpdateEl = document.getElementById(formIdUpdate);
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
@@ -13,7 +14,41 @@ class UserController {
 
             this.showPanelCreate();
 
-        })  
+        });
+
+        this.formUpdateEl.addEventListener('submit', e=>{
+            
+            event.preventDefault();
+            
+            let btn = this.formUpdateEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues(this.formUpdateEl);
+
+            let index = this.formUpdateEl.dataset.trIndex;
+
+            let tr = this.tableEl.rows[index];
+
+            tr.dataset.user = JSON.stringify(values);
+
+            tr.innerHTML = `
+                <td><img src="${values.photo}" alt="User Image" class="img-circle img-sm"></td>
+                <td>${values.name}</td>
+                <td>${values.email}</td>
+                <td>${(values.admin) ? 'Sim' : 'Não'}</td>
+                <td>${Utils.dateFormat(values.register)}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            `;
+
+            this.addEventsTr(tr);
+
+            this.updateCount();
+
+        })
 
     }
 
@@ -25,7 +60,7 @@ class UserController {
             let btn = this.formEl.querySelector("[type=submit]")
             btn.disabled = true;
 
-            let values = this.getValues();
+            let values = this.getValues(this.formEl);
 
             if(!values) return false;
 
@@ -77,11 +112,11 @@ class UserController {
         });
     }
 
-    getValues() {
+    getValues(formEl) {
         let user = {};
         let isValide = true;
 
-        [...this.formEl.elements].forEach(function (field, index) {
+        [...formEl.elements].forEach(function (field, index) {
 
             if(['name' , 'email', 'password'].indexOf(field.name) > -1 && !field.value ){
 
@@ -137,11 +172,22 @@ class UserController {
             </td>
         `
 
-        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+       this.addEventsTr(tr);
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+
+    }
+
+    addEventsTr(tr){
+         tr.querySelector(".btn-edit").addEventListener("click", e=>{
 
             let json = JSON.parse(tr.dataset.user);
 
             let form = document.querySelector('#form-user-update');
+
+            form.dataset.trIndex = tr.sectionRowIndex;
 
             for (let name in json){
                 let field = form.querySelector('[name=' + name.replace('_', '') + ']');
@@ -172,12 +218,7 @@ class UserController {
 
             this.showPanelUpdate();
 
-        })
-
-        this.tableEl.appendChild(tr);
-
-        this.updateCount();
-
+        });
     }
 
     showPanelCreate(){
